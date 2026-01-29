02a5a215a80f6f662ea1c2ad0ac85c540ac03502
mma <mma@mma-MacBook-Air.local>
2026-01-29 23:02:33 +0300

    Debug: Fix TS errors in isolated auth.ts

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,3 +1,5 @@
+import { NextAuthOptions } from "next-auth";
+// import { PrismaAdapter } from "@next-auth/prisma-adapter";
 // import prisma from "@/lib/prisma";
 import CredentialsProvider from "next-auth/providers/credentials";
 // import bcrypt from "bcryptjs";
